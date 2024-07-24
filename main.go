51c9eb9bd656ec15a5a71b30061657f13beff819
Franklin <71532004+fran1014@users.noreply.github.com>
2024-07-24 21:57:13 +0200

    created the  routes

--- main.go
+++ main.go
@@ -17,3 +17,42 @@ type User struct {
 	Name    string `json:"name"`
 	Email   string `json:"email"`
 }
+
+//main function
+func main(){
+	//connect to database
+	db, err := sql.Open("postgress", os Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	//create table if not exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT, email TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//create router
+	router := mux NewRouter()
+	router.HandleFunc("/api/go/users", getUsers(db)).Methods("GET")
+	router.HandleFunc("/api/go/users", createUser(db)).Methods("POST")
+	router.HandleFunc("/api/go/users/{id}", getUser(db)).Methods("GET")
+	router.HandleFunc("/api/go/users/{id}", updateUser(db)).Methods("PUT")
+	router.HandleFunc("/api/go/users/{id}", deleteUser(db)).Methods("DELETE")
+
+	// wrap the router with CORS and JSON content type middlewares
+	enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
+
+	//start server 
+	log.Fatal(http.ListenAndServe(":8000", enhancedRouter))
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	retun http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)
+    //Set CORS headers
+	w.Header().Set("Access-Control-Allow-Origin", "*") //Allow any origin
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS ") 
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+)
+}
